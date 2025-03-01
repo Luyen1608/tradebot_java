19735825be63573779f09c2bb98566b825256a6d
Luyen1608 <96036620+Luyen1608@users.noreply.github.com>
2025-03-01 14:19:03 +0700

    create

---
+++ src/test/java/luyen/tradebot/Trade/TradeApplicationTests.java
@@ -0,0 +1,13 @@
+package luyen.tradebot.Trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
